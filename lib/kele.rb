e3a9609cfdc01cf4ac91e938595769d725b159cc
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-07-27 20:00:08 -0400

    messaging

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,32 +8,60 @@ class Kele
   def initialize(email, password)
     @endpoint = 'https://www.bloc.io/api/v1'
 
-    response =  Kele.post(
+    response = Kele.post(
       "#{@endpoint}/sessions",
       body: { email: email, password: password }
     )
 
     if response && response["auth_token"]
-      # great!
       @auth_token = response["auth_token"]
       puts "#{email} was successfully logged in"
     else
-      # invalid login
       puts "Login invalid"
     end
   end
 
   def get_me
     url = "#{@endpoint}/users/me"
-    response(url)
+
+    response = Kele.get(url, headers: { "authorization" => @auth_token })
   end
 
   def get_mentor_availability
     url = "#{@endpoint}/mentors/539470/student_availability"
-    response(url)
+
+    response = Kele.get(url, headers: { "authorization" => @auth_token })
   end
 
-  def response(url)
-    Kele.get(url, headers: { "authorization" => @auth_token })
+  def get_messages(*page)
+    url = "#{@endpoint}/message_threads"
+
+    if page == []
+      response = Kele.get(url, {headers: { "authorization" => @auth_token }})
+    else
+      response = Kele.get("#{url}?page=#{page[0]}", {headers: { "authorization" => @auth_token }})
+    end
+
+    JSON.parse(response.body)
+  end
+
+  def create_message
+    response = Kele.post(
+      "#{@endpoint}/messages",
+      headers: {
+        authorization: @auth_token
+      },
+      body: {
+        sender: "joseph.hakoopian@gmail.com",
+        recipient_id: 539470,
+        subject: "test message",
+        "stripped-text" => "trying to get this to work"
+      }
+    )
+    if response.success?
+      puts "Nicely done"
+    else
+      puts "Whoops - response: #{response.inspect}"
+    end
   end
 end
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,11 +1,13 @@
 module Roadmap
   def get_roadmap(roadmap_id)
     url = "#{@endpoint}/roadmaps/#{roadmap_id}"
-    response(url)
+
+    response = Kele.get(url, headers: { "authorization" => @auth_token })
   end
 
   def get_checkpoint(checkpoint_id)
     url = "#{@endpoint}/checkpoints/#{checkpoint_id}"
-    response(url)
+
+    response = Kele.get(url, headers: { "authorization" => @auth_token })
   end
 end
